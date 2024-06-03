5e7b7c18537d720500c2be01dd3e848e52431b14
VasiliyYarosh <vasilliy.yarosh@gmail.com>
2024-06-03 22:31:21 +0300

    makin sceleton

---
+++ src/components/BattleFieldComponent.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+// import 
+import CardComponent from './CardComponent';
+
+interface IBattleFieldComponentProps {
+
+}
+
+const BattleFieldComponent: React.FC<IBattleFieldComponentProps> = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BattleFieldComponent;
---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+// import 
+// import CardComponent from './CardComponent';
+
+interface ICardComponentProps {
+
+}
+
+const CardComponent: React.FC<ICardComponentProps> = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CardComponent;
